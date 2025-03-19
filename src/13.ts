9e0f5edc5c600256bf624d64e1f2b7ca4d4013e0
dabrowskifestus <dabrowskifestus@gmail.com>
2025-03-19 12:36:09 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,6 @@
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
+// Example usage:
+console.log(getRandomInt(10)); // Outputs a random integer between 0 and 9, inclusive.
