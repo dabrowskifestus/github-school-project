644fa4575bedabeba10f35708a8196ac0b745f26
dabrowskifestus <dabrowskifestus@gmail.com>
2025-04-01 14:28:41 +0000

    Add 20.ts

---
+++ src/20.ts
@@ -0,0 +1,13 @@
+import { randomBytes } from "crypto";
+
+function createRandomString(length: number): string {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return result;
+}
+
+const randomString = createRandomString(10);
+console.log(randomString);
