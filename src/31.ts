09db353182babc00e862c0a4976e9ded6e9bcaa6
dabrowskifestus <dabrowskifestus@gmail.com>
2025-04-24 04:06:50 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from '@antv/map-data-utils';
+
+export const messages = defineMessages({
+  "id": {
+    "zh-cn": "ID",
+    "en-gb": "ID"
+  },
+  "country": {
+    "zh-cn": "Country Code",
+    "en-gb": "Country Code"
+  }
+});
